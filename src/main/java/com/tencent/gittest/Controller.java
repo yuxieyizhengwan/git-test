ba1bfcdee6eba07d848b80c6049d9531fda84a05
guanhaoshuai <786925036@qq.com>
2021-10-12 15:16:13 +0800

    first msg

---
+++ src/main/java/com/tencent/gittest/Controller.java
@@ -0,0 +1,11 @@
+package com.tencent.gittest;
+
+/**
+ * @author GuanJiaHe
+ * @version 1.0
+ * @description TODO
+ * @date 2021/10/12 15:09
+ */
+public class Controller {
+
+}
---
+++ src/main/java/com/tencent/gittest/GitTestApplication.java
@@ -0,0 +1,14 @@
+package com.tencent.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hahahah");
+        SpringApplication.run(GitTestApplication.class, args);
+    }
+
+}
